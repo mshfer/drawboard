b518d0a3249768e8e047b865fbce57a7b66bca1d
Marius Schaefer <marius.schaefer96@gmx.de>
2018-01-19 21:52:07 +0100

    added client and server dependencies

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// store the function in a variable called express
+var express = require('express');
+// store the result of the function in a variable called app
+var app = express();
+// listen to port 3000 on localhost
+var server = app.listen(3000);
+// host everything in 'server'-folder accessible from the clients
+app.use(express.static('../client'));
