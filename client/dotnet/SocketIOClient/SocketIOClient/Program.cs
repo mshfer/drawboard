8ebc1a39ffafbf47c411d4a6ab5e8ceedfd2465f
Marius Schaefer <marius.schaefer96@gmx.de>
2018-01-30 18:22:07 +0100

    added C# project

---
+++ client/dotnet/SocketIOClient/SocketIOClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketIOClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketIOHandler socketHandler = new SocketIOHandler("http://localhost:3000");
+        }
+    }
+}
---
+++ client/dotnet/SocketIOClient/SocketIOClient/SocketIOHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketIOClient
+{
+    class SocketIOHandler
+    {
+        private String IPAdress;
+
+        public SocketIOHandler(String IPAdress)
+        {
+            this.IPAdress = IPAdress;
+        }
+
+        public void connect()
+        {
+
+        }
+
+        public void disconnect()
+        {
+
+        }
+
+        public void sendMsg()
+        {
+        
+        }
+
+    }
+}
